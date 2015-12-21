4fd785897f2efefba6d23c8219c2cbf3ba2410fe
Taichi Sudo <taichi.sudou@gmail.com>
2015-12-21 11:35:52 +0900

    Update reAssignMaterial.py

--- reAssignMaterial.py
+++ reAssignMaterial.py
@@ -17,9 +17,11 @@ for obj in sel:
     // プレフィックスが何文字かを手入力
     // 次のバージョンで自動化予定
     refShapeNode = nsMaya + abcShapeNode[0][15:]
-    print( "abc:" + abcShapeNode[0] )
-    print( "refShapeNode:" + refShapeNode )
-    print( "SG:" + sg[0] )
+    
+    // デバッグ用出力
+//    print( "abc:" + abcShapeNode[0] )
+//    print( "refShapeNode:" + refShapeNode 
+//    print( "SG:" + sg[0] )
 
     sg = cmds.listConnections( refShapeNode, s = False, d = True, type = 'shadingEngine')
     cmds.sets( str(abcShapeNode[0]),  fe = sg[0] )
